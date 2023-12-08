193e8541d58b4326cdafbdd876bc8653522d804a
Yuriy Kulikov <kulikov-y@ya.ru>
2023-12-08 17:23:00 +0300

    пункты 5-8 задания урока1

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-console.log('Hello world');
-//комментарий
+let title=10;
+let screens=20;
+let screenPrice=30;
+const rollback=15;
+const fullPrice=25;
+const adaptive=35;
+alert ("Hello.This firs homework");
+console.log("Hello-cosole");
