45c129117d8b83f66edcabf77d8cddb99a0aefc2
Yuriy Kulikov <kulikov-y@ya.ru>
2023-12-10 23:20:29 +0300

    финиш по заданию 2 урока

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ console.log("п2.2: " + screens.length); // вып. п 2.2
 // вып. п 2.3 (начало)
 
 const KursDoll = 92; //задаю курс доллара
-console.log(Math.round(KursDoll));
 
 const KursGriv = 25; //задаю курс гривны
 
@@ -27,14 +26,17 @@ const KursYuan = 12.8; //задаю курс юани
 
 // п. 2.3 вывод стоимости верстки экранов
 
-console.log("п2.3: Стоимость верстки экранов " + screenPrice.toFixed(2) + " рублей/ " + (screenPrice/KursDoll).toFixed(2) + " долларов/ " + (screenPrice/KursGriv).toFixed(2) + " гривен/ " + (screenPrice/KursYuan).toFixed(2) + " юаней ");
+console.log("п2.3: Стоимость верстки экранов " + screenPrice.toFixed(2) + " руб./ " + (screenPrice/KursDoll).toFixed(2) + " дол./ " + (screenPrice/KursGriv).toFixed(2) + " гр./ " + (screenPrice/KursYuan).toFixed(2) + " юан. ");
 
 // п. 2.3 вывод стоимости разработки сайта
 
-console.log("п2.3: Стоимость разработки сайта " + fullPrice.toFixed(2) + " рублей/ " + (fullPrice/KursDoll).toFixed(2) + " долларов/ " + (fullPrice/KursGriv).toFixed(2) + " гривен/ " + (fullPrice/KursYuan).toFixed(2) + " юаней ");
-
+console.log("п2.3: Стоимость разработки сайта " + fullPrice.toFixed(2) + " рублей/ " + (fullPrice/KursDoll).toFixed(2) + " дол./ " + (fullPrice/KursGriv).toFixed(2) + " гр./ " + (fullPrice/KursYuan).toFixed(2) + " юан. ");
 // вып. п 2.3 окончание
 
-alert ("Hello.This second homework");
+console.log(screens.toLowerCase().split(", ")); // п 2.4 
 
-console.log("Hello-cosole");
+console.log("Откат посреднику " + rollback + "%: " + (fullPrice*(rollback/100)).toFixed(2) + " руб."); // п 2.5 
+
+//alert ("Hello.This second homework");
+
+//console.log("Hello-cosole");
