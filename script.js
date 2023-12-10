7f4b78c7191f950ae3da220199ea6441bbf5eede
Yuriy Kulikov <kulikov-y@ya.ru>
2023-12-10 21:38:46 +0300

    п.1,2 задания без массива и отката посреднику

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-let title = 10;
-let screens = 20;
+let title = "Мой проект";
+console.log("п 2.1 переменная title. тип: " + typeof(title)); // вып. п 2.1
+
+let screens = "Простые, Сложные, Интерактивные";
+
 let screenPrice = 30;
+
 const rollback = 15;
-const fullPrice = 25;
-const adaptive = 35;
-alert ("Hello.This firs homework");
+
+const fullPrice = 50;
+
+
+
+
+
+console.log("п 2.1 переменная fullPrice. тип: " + typeof(fullPrice)); // вып. п 2.1переменная fullPrice. тип: " + typeof(fullPrice)); // вып. п 2.1
+
+const adaptive = true;
+
+
+
+
+
+console.log("п 2.1 переменная adaptive. тип: " + typeof(adaptive)); // вып. п 2.1
+
+console.log("п2.2: " + screens.length); // вып. п 2.2
+
+// вып. п 2.3 (начало)
+
+const KursDoll = 91.64; //задаю курс доллара
+
+const KursGriv = 2; //задаю курс гривны
+
+const KursYuan = 10; //задаю курс юани
+
+// п. 2.3 вывод стоимости верстки экранов
+
+console.log("п2.3: Стоимость верстки экранов " + screenPrice + " рублей/ " + screenPrice/KursDoll + " долларов/ " + screenPrice/KursGriv + " гривен/ " + screenPrice/KursYuan + " юаней ");
+
+// п. 2.3 вывод стоимости разработки сайта
+
+console.log("п2.3: Стоимость разработки сайта " + fullPrice + " рублей/ " + Math.round (fullPrice/KursDoll) + " долларов/ " + fullPrice/KursGriv + " гривен/ " + fullPrice/KursYuan + " юаней ");
+
+// вып. п 2.3 окончание
+
+alert ("Hello.This second homework");
+
 console.log("Hello-cosole");
-//
