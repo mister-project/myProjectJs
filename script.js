0f3573875eb58b22dd225e76b53866f43ce2900f
Yuriy Kulikov <kulikov-y@ya.ru>
2023-12-29 22:03:48 +0300

    финал урока 8 с доп. заданием

--- script.js
+++ script.js
@@ -4,119 +4,157 @@
 
 const appData = {
     title: '',
-    screens: '',
+    screens: [],
     screenPrice: 0,
     adaptive: true,
     rollback: 15,
     allServicePrices: 0,
     fullPrice: 0,
     servicePercentPrice: 0,
-    service1: '',
-    service2: '',
+    services: {},
+   
     //num: 0,
-    n: 0,
-    sum: 0,
+    
+   // sum: 0,
     
     //price: 0,
     rollbackMessage: 0,
     start: function () {
         appData.asking()
-        appData.allServicePrices = appData.getallServicePrices()
-        appData.fullPrice = appData.getFullPrice();
-        appData.servicePercentPrice = appData.getServicePercentPrices();
-        appData.title = appData.getTitle()
-        appData.rollbackMessage = appData.getRollbackMessage(appData.fullPrice)
+        appData.addPrices()
+        appData.getFullPrice()
+        appData.getServicePercentPrices()
+        appData.getTitle()
+        appData.getRollbackMessage(appData.fullPrice)
        
-        appData.logger()                   
+        appData.logger() 
+                         
       
       },
-         //isNumber()) функция проверки на чило
-    isNumber: function(num) {
-        return !isNaN(parseFloat(num) && isFinite(num))
-        
-        },
+         
 
     asking: function() {
-        appData.title = prompt('Как назывется ваш проект?', "Калькулятор верстки");
-        appData.screens = prompt('Какие типы экранов нужно разработать? (через запятую, пожалуйста)', "Простые, сложные");
-       
-        do {appData.screenPrice = +prompt('Сколько будет стоить данная работа (в руб.)?', 31000).trim()
-        } while (!appData.isNumber(appData.screenPrice));
-       
-           
-        appData.adaptive = confirm('Нужен ли адаптив на сайте? (выберите нужную кнопку)');
         
+        do {
+        appData.title = prompt('Как назывется ваш проект?', "Калькулятор верстки");
+        } while (appData.isNumber(appData.title));
+        // do {
+        //     price = prompt('Сколько будет стоить данная работа (в руб.)?', 31000).trim() //убрал + как у Саши
+        // } while (isNumber(price));
+
+        // изменения ур. 8 фОРМИРУЕМ новые циклы, массивы, обьекты(ниже)
+        for (let i = 0; i < 2; i++) {
+            do {
+            name = prompt('Какие типы экранов нужно разработать?' )
+            }  while (appData.isNumber(name));
+            let price = 0;
+
+            do {
+                price = prompt('Сколько будет стоить данная работа (в руб.)?', 31000).trim() //убрал + как у Саши
+            } while (!appData.isNumber(price));
+
+            appData.screens.push({ id: i, name: name, price: price })
+        }
+
+       
+        
+
+        //ур8. новые циклы конец
+
+        //Начало цикла for
+        for (let i = 0; i < 2; i++) {
+            do {
+            name = prompt('Какой дополнительный тип услуги нужен?', 'услуга1')  
+        }  while (appData.isNumber(name));  
+            let price = 0;            
+             
+            do {
+            
+            price = prompt('Сколько это будет стоить (руб.)?', 4500);   
+            } while (!appData.isNumber(price)) 
+              
+               
+               appData.services[name[i]] = +price
+            }          
+          
+                appData.adaptive = confirm('Нужен ли адаптив на сайте? (выберите нужную кнопку)');        
+    
+},
+
+
+//isNumber()) функция проверки на чило
+isNumber: function(num) {
+    return !isNaN(parseFloat(num) && isFinite(num))
+    
     },
+        addPrices: function() {
+           
+            //Считаем суммарную стоимость экранов в массиве screens через reduce        
+           appData.screenPrice = appData.screens.reduce(function(sum, item) {      
+            return sum + (+item.price)   
+          }, 0)
+          
+             //метод - цикл для ввода и валидации доп. услуг
+            for(let key in appData.services){
+            appData.allServicePrices += appData.services[key]
+        }
+    },
+
+    
 //В названии проекта удаление пробелов + отделение первой буквы, превращение в заглавную и присоединении остальной части названия проекта
     getTitle: function() {
-        return ((appData.title.trimStart()).toUpperCase()).slice(0,1) + appData.title.trimStart().toLowerCase().substring(1)
+        appData.title = +((appData.title.trimStart()).toUpperCase()).slice(0,1) + appData.title.trimStart().toLowerCase().substring(1)
         
       },
    
-    //метод - цикл для ввода и валидации доп. услуг 
-    getallServicePrices: function() {
-              
     
-            for (let i = 0; i < 2; i++) {
-                
-                if (i === 0) {
-                    appData.service1 = prompt('Какой дополнительный тип услуги нужен?', 'услуга1')
-                         
-                } else if (i===1) {
-                    appData.service2 = prompt('Какой ещё дополнительный тип услуги нужен?', 'услуга2');
-                    
-                }
-                 
-                do {
-                
-                appData.n = prompt('Сколько это будет стоить (руб.)?', 4500);   
-                } while (!appData.isNumber(appData.n)) {
-                   // console.log(sum);  
-                   
-                   appData.sum += +appData.n;
-                }       
-                
-              
-               
-            }
-                return appData.sum
-                
-           
-        },
+   
       //метод для сложения итоговой стоимости (без отката)
       getFullPrice: function() {
-        return appData.screenPrice + appData.allServicePrices;
+        appData.fullPrice = appData.screenPrice + appData.allServicePrices;
       },
 
       //метод подсчета стоимости за вычетом отката
       getServicePercentPrices: function() {
-        return appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
+        appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
     },
     // Расчет скидки в зависимости от суммы
     getRollbackMessage: function(price) {
         switch (true) {
             case price > 30000:
-                return 'Даем скидку 10%';
+                appData.rollbackMessage = 'Даем скидку 10%';
                 break;
             case 15000 < price && price <= 30000:
-                return 'Даем скидку 5%';
+                appData.rollbackMessage = 'Даем скидку 5%';
                 break;
             case 0 < price && price <= 15000:
-                return 'Скидка не предусмотрена';
+                appData.rollbackMessage = 'Скидка не предусмотрена';
                 break;
             case price  <= 0:
-                return 'Что-то пошло не так';
+                appData.rollbackMessage = 'Что-то пошло не так';
                 break;
         }
         
     },  
 
     logger: function () {
+        //вывод состава метода appData
         for (const key in appData) {
-          console.log("Ключ: " + key + " значение: " + appData[key]);
+          
         }
+
+        
+        
+
+        console.log(appData.fullPrice);
+        console.log(appData.servicePercentPrice);
+        console.log(appData.screens); 
+         //вывод в консоль общей стоимости экранов       
+        console.log('Экраны_сумма: ' + appData.screenPrice);
+
       },
-    
+     
+      
       // --Блок функционала--
      
     };
@@ -126,13 +164,12 @@ const appData = {
   
 
 // функционал
-
-appData.start();      
+appData.start(); 
 
 
 //блок вывода(мусор)
+// const result = screens.reduce(function(sum, item)) {
+//     return sum + item.price
+// }, 0)
 
-
-
-
-
+// console.log(result);
