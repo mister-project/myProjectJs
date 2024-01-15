2cee2bd74435c74ef4de0f299df9a44151a80e0e
Yuriy Kulikov <kulikov-y@ya.ru>
2024-01-15 22:00:27 +0300

    Урок12 кнопка - еще одна попытка

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 const h1 = document.getElementsByTagName("h1")[0];
- 
+
 //ур9. п2
 const buttonsBtn = document.getElementsByClassName("handler_btn")[0];
 //ур. 9 п.3
@@ -21,9 +21,11 @@ const fullTotalCount = document.getElementsByClassName('total-input')[3]
 const totalCountRollback = document.getElementsByClassName('total-input')[4]
 // ур 9. п.8
 let screens = document.querySelectorAll(".screen");
- 
+
+
 //объявление переменных
- 
+
+
 const appData = {
     title: '',
     screens: [],
@@ -37,15 +39,13 @@ const appData = {
     servicesPercent: {},
     servicesNumber: {},
     rollbackMessage: 0,
- 
+
     init: function () {
         appData.addTitle()
-        appData.isError = false
-       console.log('appData.isError: ' + appData.isError)
+        
         buttonsBtn.addEventListener('click', appData.start)
         buttonsPlus.addEventListener('click', appData.addScreenBlock) 
     },
-   
     addTitle: function() {
         document.title = title.textContent
     },
@@ -70,54 +70,34 @@ const appData = {
         totalCountOther.value = appData.servicePricesPercent + appData.servicePricesNumber
         fullTotalCount.value = appData.fullPrice
       },
-  //appData.isError = false;  
-  
-   addScreens: function() {
-   screens = document.querySelectorAll(".screen")
-   screens.forEach(function(screen, index) {
-   const select = screen.querySelector('select');
-   const input = screen.querySelector('input')
-   const selectName = select.options[select.selectedIndex].textContent
-   console.log('select.value: '+ +select.value)
-   
-  
-//
-  screen.length = 1;
-  screens = document.querySelectorAll('.screen');
-  for (let i = 0; i < screen.length; i++) {
-    if (select.value.trim().length === 0 || input.value.trim().length === 0) {
-      appData.isError = true;
-     
-    }
-else {
- 
-  screens = document.querySelectorAll('.screen');
-      appData.screens.push({ 
+       
+    addScreens: function() {
+        screens = document.querySelectorAll(".screen")
+screens.forEach(function(screen, index) {
+    const select = screen.querySelector('select');
+    const input = screen.querySelector('input')
+    const selectName = select.options[select.selectedIndex].textContent
+
+    appData.screens.push({ 
         id: index, 
         name: selectName, 
         price: +select.value * +input.value
-    }) 
-}
-    console.log('appData.isError: ' + appData.isError)
-}
-//
-  
-  
+    })   
 })
-console.log('appData.screens' + appData.screens);
- }, 
- 
+
+    }, 
+
     addServices: function() {
         otherItemsPercent.forEach(function (item){
             const check = item.querySelector('input[type=checkbox]')
             const label = item.querySelector('label')
             const input = item.querySelector('input[type=text]')
-            // console.log(check);
-            // console.log(label);
-            // console.log(input);
+            console.log(check);
+            console.log(label);
+            console.log(input);
             if(check.checked) {
                 appData.servicesPercent[label.textContent] = +input.value
- 
+
             }   
             
         })
@@ -125,23 +105,23 @@ console.log('appData.screens' + appData.screens);
             const check = item.querySelector('input[type=checkbox]')
             const label = item.querySelector('label')
             const input = item.querySelector('input[type=text]')
-            // console.log(check);
-            // console.log(label);
-            // console.log(input);
+            console.log(check);
+            console.log(label);
+            console.log(input);
             if(check.checked) {
                 appData.servicesNumber[label.textContent] = +input.value
- 
+
             }               
         })      
- 
+
     },
- 
+
     addScreenBlock: function() {
         const cloneScreen = screens[0].cloneNode(true)
        
         screens[screens.length - 1].after(cloneScreen)
     },
- 
+
     addPrices: function() {
            
             //Считаем суммарную стоимость экранов в массиве screens через reduce        
@@ -153,14 +133,15 @@ console.log('appData.screens' + appData.screens);
             for(let key in appData.servicesNumber){
             appData.servicePricesNumber += appData.servicesNumber[key]
         }
- 
+
         for(let key in appData.servicesPercent){
             appData.servicePricesPercent += appData.screenPrice *(appData.servicesPercent[key]/100);
         }
- 
+
         appData.fullPrice = +appData.screenPrice + appData.servicePricesNumber + appData.servicePricesPercent;
     },
- 
+
+
       //метод подсчета стоимости за вычетом отката
       getServicePercentPrices: function() {
         appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
@@ -183,7 +164,7 @@ console.log('appData.screens' + appData.screens);
         }
         
     },  
- 
+
     logger: function () {
         //вывод состава метода appData
         for (const key in appData) {
@@ -191,12 +172,12 @@ console.log('appData.screens' + appData.screens);
         }
      
         
-        // console.log(appData.fullPrice);
-        // console.log(appData.servicePercentPrice);
-        // console.log(appData.screens); 
-        //  //вывод в консоль общей стоимости экранов       
-        // console.log('Экраны_сумма: ' + appData.screenPrice);
- 
+        console.log(appData.fullPrice);
+        console.log(appData.servicePercentPrice);
+        console.log(appData.screens); 
+         //вывод в консоль общей стоимости экранов       
+        console.log('Экраны_сумма: ' + appData.screenPrice);
+
       },
      
       
@@ -207,7 +188,8 @@ console.log('appData.screens' + appData.screens);
    
  //КОНЕЦ ОБЬЕКТА appData    
   
- 
+
 // функционал
 appData.init(); 
- 
+
+
